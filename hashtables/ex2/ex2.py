70185453cdb93f0a103062e766a22101efc3e2b2
lydiecherilus <lydiecherilus@yahoo.fr>
2020-08-14 21:48:52 -0400

    ex1, ex2, ex4

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,18 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    cache = dict()
+    # flights
+    route = [None] * length
+
+    # go through tickets, link source to destination
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+    # ticket source is next ticket destination
+    next = cache['NONE']
+
+    for flight in range(length):
+        route[flight] = next
+        next = cache[next]
 
     return route
